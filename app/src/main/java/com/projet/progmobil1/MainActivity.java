7421e996a49ecfa9763066d0bc5b87f674c75c28
dorian <dorianrey10@gmail.com>
2026-02-03 15:55:08 +0100

    1

--- app/src/main/java/com/projet/progmobil1/MainActivity.java
+++ app/src/main/java/com/projet/progmobil1/MainActivity.java
@@ -1,24 +1,17 @@
 package com.projet.progmobil1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "TEST_HONOR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnJava = findViewById(R.id.btnVersionJava);
-        btnJava.setOnClickListener(v -> {    Intent intent = new Intent(MainActivity.this, JavaFormulaireActivity.class);
-            startActivity(intent);
-        });
 
         Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
     }
